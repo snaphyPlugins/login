23aab13cc866eb5568b5b0f7c33603b6c36c30a5
robinskumar73 <robinskumar73@gmail.com>
2015-12-02 07:26:45 +0530

    Login method done

--- client/scripts/controllers/login.js
+++ client/scripts/controllers/login.js
@@ -2,11 +2,31 @@
 /*global angular, $snaphy, $scope, $stateParams */
 
 angular.module($snaphy.getModuleName())
-.controller('loginControl', ['$scope', 'Database',
-    function($scope, Database) {
+.controller('loginControl', ['$scope', 'Database', '$location', 'LoginServices', '$injector',
+    function($scope, Database, $location, LoginServices, $injector) {
         //Controller defined here..
         $snaphy.template = false;
-        var userService = Database.getDb('login', 'User');
+        var UserService = Database.getDb('login', 'User');
+        $scope.credentials = {};
+
+        $scope.login = function(){
+            //Now login to the employee ..
+            UserService.login($scope.credentials, function(){
+                //If redirected from 401 error..
+                if($location.nextAfterLogin){
+                    $location.path('/');
+                    $location.nextAfterLogin = null;
+                }else{
+                    var $state = $injector.get('$state');
+                    //Redirect to the default State..
+                    $state.go(LoginServices.redirectOtherWise);
+                }
+            },function(){
+                //TODO SHOW LOGIN FAILURE ON CONSOLE..
+                console.error("Error getting logged in.");
+            });
+        };
+
         
 
     }//controller function..
