5b95b6cb12fd1282e34ab02fbcc414ef3169262c
robinskumar73 <robinskumar73@gmail.com>
2015-12-07 20:52:06 +0530

    State change on 401 error bug solved

--- client/scripts/controllers/login.js
+++ client/scripts/controllers/login.js
@@ -3,8 +3,8 @@
 
 angular.module($snaphy.getModuleName())
 
-.controller('loginControl', ['$scope', 'Database', '$location', 'LoginServices', '$injector',
-    function($scope, Database, $location, LoginServices, $injector) {
+.controller('loginControl', ['$scope', 'Database', '$rootScope', 'LoginServices', '$injector',
+    function($scope, Database, $rootScope, LoginServices, $injector) {
         //Adding title and name..
         $scope.name = $snaphy.loadSettings('login', 'loginName');
         $scope.title = $snaphy.loadSettings('login', 'loginTitle');
@@ -12,34 +12,47 @@ angular.module($snaphy.getModuleName())
         $scope.registerState     = $snaphy.loadSettings('login', "registerState");
         $scope.forgotPassState   = $snaphy.loadSettings('login', "forgotPassState");
 
-        //Controller defined here..
-        $snaphy.setDefaultTemplate(false);
+        var defaultTemplate = $snaphy.loadSettings('login', "defaultTemplate");
+        $snaphy.setDefaultTemplate(defaultTemplate);
+
         var UserService = Database.getDb('login', 'User');
         $scope.credentials = {};
         $scope.loginError = false;
 
+        function disableButton(){
+            $scope.isClickEnabled = false;
+        }
+
+        function enableButton(){
+            $scope.isClickEnabled = true;
+        }
+        //Enable the login button at first..
+        enableButton();
         $scope.login = function(loginForm){
             if(loginForm.$valid){
+                disableButton();
                 //Now login to the employee ..
                 UserService.login($scope.credentials, function(userDetail){
+                    enableButton();
                     $scope.loginError = false;
                     console.log(userDetail.user);
                     //Add user detail to the database..
                     LoginServices.addUserDetail(userDetail.user);
-                    //If redirected from 401 error..
-                    if($location.nextAfterLogin){
-                        $location.path($location.nextAfterLogin);
-                        $location.nextAfterLogin = null;
-                    }else{
-                        var $state = $injector.get('$state');
-                        //Redirect to the default State..
-                        $state.go(LoginServices.redirectOtherWise);
-                    }
+                    var $state = $injector.get('$state'),
+                    redirectTo =  $rootScope.previousState.name ?  $rootScope.previousState.name :  LoginServices.redirectOtherWise;
+                    console.log("State changed to " + redirectTo);
+                    console.log(redirectTo);
+                    //Redirect to the default State..
+                    $state.go(redirectTo);
+
                 },function(){
+                    enableButton();
+                    $scope.errorMsg = "Please login by providing correct username and password.";
                     //Display login error..
                     $scope.loginError = true;
                     console.error("Error getting logged in.");
                 });
+
             }//if valid
 
         }; //login function
